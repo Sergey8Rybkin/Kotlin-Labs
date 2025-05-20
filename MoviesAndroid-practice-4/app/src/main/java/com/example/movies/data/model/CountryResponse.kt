1ad5d44f0cd7766719941a23586f97c1fb3fe68f
Sergey8Rybkin <100475554+Sergey8Rybkin@users.noreply.github.com>
2025-05-20 12:20:54 +0500

    Delete MoviesAndroid-practice-4 directory

--- MoviesAndroid-practice-4/app/src/main/java/com/example/movies/data/model/CountryResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movies.data.model
-
-import androidx.annotation.Keep
-
-@Keep
-data class CountryResponse(
-    val name: String?
-)
--- MoviesAndroid-practice-4/app/src/main/java/com/example/movies/domain/IMovieRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movies.domain
-
-import com.example.movies.model.Movie
-
-interface IMovieRepository {
-    suspend fun getMovie(nameSearch: String): List<Movie>
-}
